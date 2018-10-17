422063e07634add1e60ea4b72e42334f1cf1ccb4
Harald Westphal <h.westphal@gmx.de>
2018-10-17 20:18:53 +0200

    Add German translation

--- libs/core/enums.d.ts
+++ libs/core/enums.d.ts
@@ -1,39 +1,62 @@
 declare const enum Color {
+    //% block=Black
     Black,
+    //% block=Off
     Off = 0,
+    //% block=Pink
     Pink,
+    //% block=Purple
     Purple,
+    //% block=Blue
     Blue,
+    //% block="Light Blue"
     LightBlue,
+    //% block=Cyan
     Cyan,
+    //% block=Green
     Green,
+    //% block=Yellow
     Yellow,
+    //% block=Orange
     Orange,
+    //% block=Red
     Red,
+    //% block=White
     White,
 }
 
 declare const enum Orientation {
+    //% block=Back
     Back,
+    //% block=Up
     Up,
+    //% block=Down
     Down,
+    //% block=Left
     Left,
+    //% block=Right
     Right,
+    //% block=Front
     Front,
 }
 
 declare const enum ColorAndDistanceMode {
     //% block="Color or Distance"
     ColorOrDistance,
+    //% block=Luminosity
     Luminosity,
 }
 
 declare const enum TiltMode {
+    //% block=Simple
     Simple,
+    //% block=Precise
     Precise,
 }
 
 declare const enum RotationMode {
+    //% block=Angle
     Angle,
+    //% block=Speed
     Speed,
 }
